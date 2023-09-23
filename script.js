d8fdef1550146ce78795292d59e85f0445430d8e
Rendesing <145534566+Rendesing@users.noreply.github.com>
2023-09-23 19:26:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,15 @@ let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-  // 剛剛沒配對成功的話，就把牌蓋起來
   if (lockBoard) return;
 
-  // 避免翻同一張牌當做第二張
   if (this === firstCard) return;
   
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
     hasFlippedCard = true;
-    firstCard = this; // this => the clicked card
+    firstCard = this; 
     return;
   }
 
@@ -25,15 +23,11 @@ function flipCard() {
 }
 
 function checkForMatch() {
-  // 如果牌組配對成功 => isMatch
-  // 就不可以再點擊那組牌 => disableCards()
-  // 配對錯誤就把該牌組蓋起來 => unflipCards()
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
   isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
-  // 移除監聽事件，釋放記憶體
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
 
@@ -43,7 +37,7 @@ function disableCards() {
 function unflipCards() {
   lockBoard = true;
 
-  // 把牌蓋起來
+ 
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
