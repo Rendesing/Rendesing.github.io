ade8a39a33491a92a2a9dbe0790bcec6e1bf823a
Rendesing <145534566+Rendesing@users.noreply.github.com>
2023-09-27 17:05:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 const startTime = new Date().getTime();
 
 // 目標時間(要倒數幾秒)。
-const targetSeconds = 45;
+const targetSeconds = 30;
 
 // 初始化。
 init(targetSeconds);
